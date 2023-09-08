1992928a5f81830c0bb80e127ef423ac0ad03ce5
asepscareer <aseps.career@gmail.com>
2023-09-08 17:58:23 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+import 'sections/about_section.dart';
+import 'sections/contact_section.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  List<Tab> tabs = [
+    const Tab(
+      text: "About",
+      icon: Icon(
+        Icons.account_box,
+      ),
+    ),
+    const Tab(
+      text: "Contact",
+      icon: Icon(
+        Icons.contact_page,
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 48, 160, 211),
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+          ),
+          toolbarHeight: 255,
+          title: Column(
+            children: [
+              profilePhotos(),
+              profileName(),
+              hobbies(),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: stats(),
+              ),
+            ],
+          ),
+          bottom: TabBar(
+            tabs: tabs,
+            indicatorColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            const SingleChildScrollView(
+              child: AboutSection(),
+            ),
+            SingleChildScrollView(
+              child: contactSection(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding hobbies() {
+    return const Padding(
+      padding: EdgeInsets.only(
+        top: 5.0,
+        bottom: 5.0,
+      ),
+      child: Text(
+        "Traveller - Dreamer - Fighter",
+        style: TextStyle(
+          fontWeight: FontWeight.normal,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
+  Padding profileName() {
+    return const Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Text(
+        "Asep Saputra",
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Row stats() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Text(
+              "Photos",
+              style: TextStyle(
+                color: Color.fromARGB(255, 28, 124, 172),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              "160",
+              style: TextStyle(fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              "Followers",
+              style: TextStyle(
+                color: Color.fromARGB(255, 28, 124, 172),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              "1657",
+              style: TextStyle(fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              "Following",
+              style: TextStyle(
+                color: Color.fromARGB(255, 28, 124, 172),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              "9",
+              style: TextStyle(fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container profilePhotos() {
+    return Container(
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red,
+      ),
+      width: 105,
+      height: 105,
+      alignment: Alignment.center,
+      child: const CircleAvatar(
+        radius: 50,
+        backgroundColor: Colors.transparent,
+        backgroundImage: NetworkImage(
+          "https://picsum.photos/300/300",
+        ),
+      ),
+    );
+  }
+}
